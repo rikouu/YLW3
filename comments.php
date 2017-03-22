2b832592145800fe63c34bd0c56f8f7c441f5ca9
yalewoo <chongruyuntian@163.com>
2017-03-22 11:38:17 +0800

    comment

--- comments.php
+++ comments.php
@@ -15,6 +15,88 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
+
+function bootstrapwp_comment( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment;
+	switch ( $comment->comment_type ) :
+		case 'pingback' :
+			break;
+		case 'trackback' :
+	  // 用不同于其它评论的方式显示 trackbacks 。
+	?>
+	<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
+		<p><?php _e( 'Pingback:', 'bootstrapwp' ); ?> <?php comment_author_link(); ?> <?php edit_comment_link( __( '(Edit)', 'bootstrapwp' ), '<span class="edit-link">', '</span>' ); ?>
+		</p>
+	<?php
+		break;
+		default :
+		// 开始正常的评论
+		global $post;
+	 ?>
+	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+		<div id="comment-<?php comment_ID(); ?>" class="comment-one">
+			<div class="comment-author-avatar">
+  			<?php // 显示评论作者头像 
+  			  echo get_avatar( $comment, 64 ); 
+  			?>
+			</div>
+			<?php // 未审核的评论显示一行提示文字
+			  if ( '0' == $comment->comment_approved ) : ?>
+  			<p class="comment-awaiting-moderation">
+  			  Your comment is awaiting moderation.
+  			</p>
+			<?php endif; ?>
+			<div class="comment-body">
+				<div class="comment-author-name">
+  				<?php // 显示评论作者名称
+  				    printf( '%1$s %2$s',
+  						get_comment_author_link(),
+  						// 如果当前文章的作者也是这个评论的作者，那么会出现一个标签提示。
+  						( $comment->user_id === $post->post_author ) ? '<span class="comment-author-is-bloger"> ' .'作者' . '</span>' : ''
+  					);
+  				?>
+  				</div>
+  				
+  				<?php // 显示评论内容
+				  comment_text(); 
+				?>
+				
+				<div class="comment-meta">
+
+    				<?php // 显示评论的发布时间
+    				    printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
+    						esc_url( get_comment_link( $comment->comment_ID ) ),
+    						get_comment_time( 'c' ),
+    					  // 翻译: 1: 日期, 2: 时间
+    						sprintf( __( '%1$s %2$s', 'fenikso' ), get_comment_date(), get_comment_time() )
+    					);
+    				?>
+
+
+				
+				<?php // 显示评论的编辑链接 
+				  edit_comment_link( __( 'Edit', 'bootstrapwp' ), '<span class="edit-link">', '</span>' ); 
+				?>
+				
+				<span class="reply">
+					<?php // 显示评论的回复链接 
+					  comment_reply_link( array_merge( $args, array( 
+					    'reply_text' =>  '回复', 
+					    'depth'      =>  $depth, 
+					    'max_depth'  =>  $args['max_depth'] ) ) ); 
+					?>
+				</span>
+				</div>
+			</div>
+		</div>
+	<?php
+		break;
+	endswitch; // end comment_type check
+}
+?>
+
+
+<?php
 if ( post_password_required() ) {
 	return;
 }
@@ -51,16 +133,20 @@ if ( post_password_required() ) {
 	$req = get_option( 'require_name_email' );
 	$aria_req = ( $req ? " aria-required='true'" : '' );
 	$fields =  array(
-		'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-			'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
-		'email'  => '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-			'<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>',
-	);
+        'author' => '<p class="comment-form-author">' . ( $req ? '<span class="required">*</span>' : '' ) .
+            '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /> <label for="author">' . ' -> 你的姓名' . '</label> </p>',
+        'email'  => '<p class="comment-form-email">' . ( $req ? '<span class="required">*</span>' : '' ) .
+            '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /><label for="email">' . ' -> 你的邮箱（用于展示头像和回复邮件提醒）' . '</label> </p>',
+        'url'    => '<p class="comment-form-url">' .
+            '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30"' . $aria_req . '  /><label for="url">' . ' -> 网站' . '</label></p>',
+    );
 	 
 	$comments_args = array(
 		'fields' =>  $fields,
-		'title_reply'=>'评论一下',
-		'label_submit' => '发射！'
+		'title_reply'=>'我要评论',
+		'label_submit' => '发表评论',
+		'comment_notes_before' => '',
+        'comment_field' => '<p class="comment-form-comment"><textarea id="comment" name="comment" aria-required="true"></textarea></p>'
 	);
 	 
 	comment_form($comments_args);
